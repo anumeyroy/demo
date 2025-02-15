382ed229561eeef90910174f275bd355843dc61f
Anumey Roy <anumeyroy5@gmail.com>
2025-02-15 15:30:17 +0530

    add utills classes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import dotEnv from 'dotenv';
 import express from 'express';
-import cluster from 'node:cluster';
+// import cluster from 'node:cluster';
 import chalk from 'chalk';
 import connectMongoDB from './db/connectdb.js';
 
 const app = express();
 dotEnv.config();
-connectMongoDB();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(chalk.bgGreen.bold(`server is listning on ${port}`));
-});
+connectMongoDB()
+  .then(() =>
+    app.listen(port, () => {
+      console.log(chalk.bgGreen.bold(`server is listning on ${port}`));
+    })
+  )
+  .catch((error) => console.log(chalk.bgRed('Failed to connect mongoDB')));
